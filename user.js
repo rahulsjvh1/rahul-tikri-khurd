829f33df843a91a4c05ad113944310045e1a8331
rahulsjvh1 <152975588+rahulsjvh1@users.noreply.github.com>
2024-01-09 21:03:04 +0530

    Update user.js
    yah Java Script security page ka hai yah aap Jab login page ya sign up page banaoge to uske andar ka jo page Ham banate hain jo ki Ham dusre ke system Mein Nahin Khulna chahie Nahin to dusre tape Mein Nahin to dusre window Mein To yah use type ka hai ismein aap jisse login kiye ho Usi Mein login Milega Agar dusre URL Mein khologe ya dusre Chrome Mein khologe To Nahin khulega Chahe Jis system Mein khole Usi ke dusre wale tab mein bhi khologe na to bhi nahin khulega jismein aapka already login Hai Usi Mein khulega

--- user.js
+++ user.js
@@ -2,4 +2,23 @@ window.onload = function () {
     if (sessionStorage.getItem("user") == null) {
         window.location.replace("testbooklogin.html");
     } else {
-       
+        var profile_upload = document.getElementById("profile_upload");
+        profile_upload.onchange = function(){
+
+        var replace = new FileReader();
+        replace.readAsDataURL(profile_upload.files[0]);
+        replace.onload = function(){
+        var filename = replace.result;
+        var profile_ico = document.getElementById("profile_ico");
+        var profile_pic = document.getElementById("profile_pic");
+        profile_pic.style.backgroundImage= "url("+filename+")";
+        profile_pic.style.backgroundSize = "cover";
+        profile_pic.style.backgroundPosition = "center";
+        profile_ico.style.display ="none";
+
+            }
+        }
+        
+    }
+    
+}
